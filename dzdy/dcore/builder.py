28f5e35e3b16ec1f50829498551db35d58b7b7bf
TimeWZ667 <timewz667@gmail.com>
2017-06-04 20:03:01 +0100

    Update script reading

--- dzdy/dcore/builder.py
+++ dzdy/dcore/builder.py
@@ -31,14 +31,14 @@ def build_from_script(scr):
         mss = [re.match(r'(?P<node>\w+)\[(?P<ms>(\w|\|)+)\]', st) for st in dcs]
         mss = {ms['node']: ms['ms'].split('|') for ms in mss if ms}
 
-        sts = [re.match(r'(?P<state>\w+)\{(?P<ms>(.*)+)\}', st) for st in dcs]
+        sts = [re.match(r'(?P<state>(\w|\|)+)\{(?P<ms>(.*)+)\}', st) for st in dcs]
         sts = {st['state']: dict(re.findall(r'(?P<a>\w+):(?P<b>\w+)', st['ms'])) for st in sts if st}
         targets = {st: list() for st in sts.keys()}
         js['Microstates'] = mss
         js['States'] = sts
 
     elif pars['Type'] == 'CTMC':
-        sts = [re.match(r'(?P<state>\A\w+\Z)', st) for st in dcs]
+        sts = [re.match(r'(?P<state>\A(\w|\|)+\Z)', st) for st in dcs]
         sts = [st['state'] for st in sts if st]
         targets = {st: list() for st in sts}
 
@@ -47,12 +47,12 @@ def build_from_script(scr):
     else:
         raise SyntaxError('Can not specify model type')
 
-    tr1 = [re.match(r'(?P<fr>\w+)\((?P<by>\w+)\)->(?P<to>\w+)', st) for st in dcs]
+    tr1 = [re.match(r'((\w|\|)+--)*(?P<fr>(\w|\|)+)\((?P<by>\w+)\)->(?P<to>(\w|\|)+)', st) for st in dcs]
     tr1 = {tr['fr']: {'Dist': tr['by'], 'To': tr['to']} for tr in tr1 if tr}
-    trs = [re.match(r'(?P<fr>\w+)->(?P<to>\w+)', st) for st in dcs]
+    trs = [re.match(r'((\w|\|)+--)*(?P<fr>(\w|\|)+)->(?P<to>(\w|\|)+)', st) for st in dcs]
     trs = {tr['fr']: {'Dist': tr['fr'], 'To': tr['to']} for tr in trs if tr}
     trs.update(tr1)
-    links = [re.match(r'(?P<fr>\w+)--(?P<to>\w+)', st) for st in dcs]
+    links = [re.match(r'(?P<fr>(\w|\|)+)--(?P<to>(\w|\|)+)', st) for st in dcs]
 
     for link in links:
         if link:
