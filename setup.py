e87d736c83d89129f4a152163993cb5c173dddd4
TimeWz667 <TimeWz667@gmail.com>
2017-06-07 20:05:04 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+
+setup(name='Kamanian',
+      version='1.00',
+      packages=['dzdy'],
+      install_requires=['pandas', 'numpy', 'scipy', 'pcore', 'matplotlib', 'networkx'])
