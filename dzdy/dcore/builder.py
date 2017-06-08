bfca92def87185abf7345236fbbeeda3e02b4c70
TimeWz667 <TimeWz667@gmail.com>
2017-06-08 10:59:20 +0100

    reformed re.groups()

--- dzdy/dcore/builder.py
+++ dzdy/dcore/builder.py
@@ -24,22 +24,22 @@ def build_from_script(scr):
     dcs = [re.sub(r'#\w+', '', st) for st in dcs if st is not '']
 
     pars = [re.match(r'(?P<par>\w+)=(?P<val>\w+)', st) for st in dcs]
-    pars = {par['par']: par['val'] for par in pars if par}
+    pars = {par.group('par'): par.group('val') for par in pars if par}
     js = {'ModelName': pars['Name'], 'ModelType': pars['Type']}
 
     if pars['Type'] == 'CTBN':
         mss = [re.match(r'(?P<node>\w+)\[(?P<ms>(\w|\|)+)\]', st) for st in dcs]
-        mss = {ms['node']: ms['ms'].split('|') for ms in mss if ms}
+        mss = {ms.group('node'): ms.group('ms').split('|') for ms in mss if ms}
 
         sts = [re.match(r'(?P<state>(\w|\|)+)\{(?P<ms>(.*)+)\}', st) for st in dcs]
-        sts = {st['state']: dict(re.findall(r'(?P<a>\w+):(?P<b>\w+)', st['ms'])) for st in sts if st}
+        sts = {st.group('state'): dict(re.findall(r'(?P<a>\w+):(?P<b>\w+)', st.group('ms'))) for st in sts if st}
         targets = {st: list() for st in sts.keys()}
         js['Microstates'] = mss
         js['States'] = sts
 
     elif pars['Type'] == 'CTMC':
         sts = [re.match(r'(?P<state>\A(\w|\|)+\Z)', st) for st in dcs]
-        sts = [st['state'] for st in sts if st]
+        sts = [st.group('state') for st in sts if st]
         targets = {st: list() for st in sts}
 
         js['States'] = sts
@@ -48,15 +48,15 @@ def build_from_script(scr):
         raise SyntaxError('Can not specify model type')
 
     tr1 = [re.match(r'((\w|\|)+--)*(?P<fr>(\w|\|)+)\((?P<by>\w+)\)->(?P<to>(\w|\|)+)', st) for st in dcs]
-    tr1 = {tr['fr']: {'Dist': tr['by'], 'To': tr['to']} for tr in tr1 if tr}
+    tr1 = {tr.group('fr'): {'Dist': tr.group('by'), 'To': tr.group('to')} for tr in tr1 if tr}
     trs = [re.match(r'((\w|\|)+--)*(?P<fr>(\w|\|)+)->(?P<to>(\w|\|)+)', st) for st in dcs]
-    trs = {tr['fr']: {'Dist': tr['fr'], 'To': tr['to']} for tr in trs if tr}
+    trs = {tr.group('fr'): {'Dist': tr.group('fr'), 'To': tr.group('to')} for tr in trs if tr}
     trs.update(tr1)
     links = [re.match(r'(?P<fr>(\w|\|)+)--(?P<to>(\w|\|)+)', st) for st in dcs]
 
     for link in links:
         if link:
-            targets[link['fr']].append(link['to'])
+            targets[link.group('fr')].append(link.group('to'))
 
     js['Transitions'] = trs
     js['Targets'] = targets
