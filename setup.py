6424424e54a7dc994b40935312fcd0d1ab67e69d
TimeWz667 <TimeWz667@gmail.com>
2017-06-15 23:52:22 +0100

    added dep in setup

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ from setuptools import setup, find_packages
 setup(name='Kamanian',
       version='1.00',
       packages=find_packages(),
-      install_requires=['pandas', 'numpy', 'scipy', 'pcore', 'matplotlib', 'networkx'])
+      install_requires=['pandas', 'numpy', 'scipy', 'pcore', 'matplotlib', 'networkx'],
+      dependency_links=['git+git://github.com/TimeWz667/PyFire.git',]
+
+
+      )
