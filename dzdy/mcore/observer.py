e881c6888efc4e5eb9443afbd8f5e7d49c7a3834
TimeWz667 <TimeWz667@gmail.com>
2017-11-27 17:42:35 +0000

    Reform observer

--- dzdy/mcore/observer.py
+++ dzdy/mcore/observer.py
@@ -1,52 +1,135 @@
 import pandas as pd
+from abc import ABCMeta, abstractmethod
 from collections import OrderedDict
 __author__ = 'TimeWz667'
 
 
-class Observer:
-    def __init__(self):
+class Observer(metaclass=ABCMeta):
+    def __init__(self, ext=True):
         self.Last = OrderedDict()
-        self.Current = self.Last
+        self.Flow = OrderedDict()
+        self.Mid = OrderedDict()
         self.TimeSeries = list()
+        self.TimeSeriesMid = list()
+        self.__ObsDt = 1
+        self.ExtMid = ext
+
+    @property
+    def ObservationalInterval(self):
+        return self.__ObsDt
+
+    @ObservationalInterval.setter
+    def ObservationalInterval(self, dt):
+        if dt > 0:
+            self.__ObsDt = dt
 
     def __getitem__(self, item):
         try:
-            return self.Current[item]
+            return self.Last[item]
         except KeyError:
             return 0
 
     def renew(self):
         self.TimeSeries = list()
+        self.TimeSeriesMid = list()
+
+    def __new_session(self, ti):
         self.Last = OrderedDict()
-        self.Current = self.Last
+        self.Last['Time'] = ti + self.ObservationalInterval
+        self.Flow.clear()
+        if self.ExtMid:
+            self.Mid = OrderedDict()
+            self.Mid['Time'] = ti + self.ObservationalInterval
 
-    def observe_fully(self, model, ti):
-        self.initialise_observation(model, ti)
-        self.update_observation(model, ti)
-        self.push_observation(ti)
+    @abstractmethod
+    def read_statics(self, mod, tab, ti):
+        """
 
-    def initialise_observation(self, model, ti):
+        :param mod: model to be observed
+        :param tab: table to store static information
+        :param ti: observation time
+        """
         pass
 
-    def update_observation(self, model, ti):
+    @abstractmethod
+    def update_dynamic_Observations(self, mod, flow, ti):
+        """
+
+        :param mod: model to be observed
+        :param flow: table to store dynamic information
+        :param ti: observation time
+        """
         pass
+
+    def initialise_observations(self, mod, ti):
+        self.renew()
+        self.Flow.clear()
+        self.read_statics(mod, self.Last, ti)
+
+    def observe_routinely(self, mod, ti):
+        self.read_statics(mod, self.Last, ti)
+        self.update_dynamic_Observations(mod, self.Flow, ti)
+
+
+    def update_at_mid_term(self, mod, ti):
+        if self.ExtMid:
+            self.read_statics(mod, self.Mid, ti)
 
     def push_observation(self, ti):
-        self.Current['Time'] = ti
-        self.TimeSeries.append(self.Current)
-        self.Last, self.Current = self.Current, OrderedDict()
+        self.Last.update(self.Flow)
+        self.TimeSeries.append(self.Last)
+        if self.ExtMid:
+            self.Mid.update(self.Flow)
+            self.TimeSeriesMid.append(self.Mid)
+        self.__new_session(ti)
+        self.Flow.clear()
+
+    def get_entry(self, i):
+        try:
+            return self.TimeSeries[i]
+        except KeyError:
+            return None
+
+    def get_entry_at(self, ti):
+        for ent in self.TimeSeries:
+            if ent['Time'] == ti:
+                return ent
+        return None
 
     @property
-    def observation(self):
+    def Observation(self):
         dat = pd.DataFrame(self.TimeSeries)
         dat = dat.set_index('Time')
         return dat
 
-    def output_csv(self, file):
-        self.observation.to_csv(file)
+    @property
+    def AdjustedObservation(self):
+        if not (self.ExtMid or len(self.TimeSeriesMid) is len(self.TimeSeries) - 1):
+            self.TimeSeriesMid = list()
+            for f, t in zip(self.TimeSeries[:-1], self.TimeSeries[1:]):
+                ent = OrderedDict()
+                for k in f.keys():
+                    ent[k] = (f[k] + t[k])/2
+                self.TimeSeriesMid.append(ent)
 
-    def output_json(self, file):
-        self.observation.to_json(file, orient='records')
+        dat = pd.DataFrame(self.TimeSeriesMid)
+        dat = dat.set_index('Time')
+        return dat
 
-    def print(self):
-        print(self.observation)
+    def output_csv(self, file, mid=False):
+        if mid:
+            self.AdjustedObservation.to_csv(file)
+        else:
+            self.Observation.to_csv(file)
+
+    def output_json(self, file, mid=False):
+        if mid:
+            self.AdjustedObservation.to_json(file, orient='records')
+        else:
+            self.Observation.to_json(file, orient='records')
+
+    def print(self, mid=False):
+        if mid:
+            print(self.AdjustedObservation)
+        else:
+            print(self.Observation)
