179282d4ff41ec13c9a1dacd839a172a46cee741
TimeWZ667 <timewz667@gmail.com>
2018-05-21 11:48:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name='PyComplexism',
       version='2.0.0',
-      packages=['complexism',
-                'complexism.misc',
-                'complexism.element'
-                'complexism.dcore',
-                'complexism.mcore',
-                'complexism.agentbased',
-                'complexism.agentbased.statespace',
-                'complexism.agentbased.diffeq',
-                'complexism.equationbased',
-                'complexism.equationbased.statespace',
-                'complexism.multimodel'],
-      install_requires=['pandas', 'nuympy', 'scipy', 'matplotlib',
+      packages= find_packages(),
+      #['complexism',
+      #          'complexism.misc',
+      #          'complexism.element',
+      #          'complexism.dcore',
+      #          'complexism.mcore',
+      #          'complexism.agentbased',
+      #          'complexism.agentbased.statespace',
+      #          'complexism.agentbased.diffeq',
+      #          'complexism.equationbased',
+      #          'complexism.equationbased.statespace',
+      #          'complexism.multimodel'],
+      install_requires=['pandas', 'numpy', 'scipy', 'matplotlib',
                         'networkx', 'PyEpiDAG'],
       )
