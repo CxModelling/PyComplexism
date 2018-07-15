b6777df30b2809c9feb4249f86f7107f1906c04e
TimeWz667@gmail.com <Ilovefh1314>
2018-07-15 08:30:15 +0100

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='PyComplexism',
-      version='3.0.3',
+      version='3.0.4',
       packages= find_packages(),
       #['complexism',
       #          'complexism.misc',
